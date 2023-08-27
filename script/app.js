53b16434d747568bf79de90365979ec016ddd842
jasmin <jasminchakma895@gmail.com>
2023-08-28 00:39:32 +0600

    call api

---
+++ script/app.js
@@ -0,0 +1,10 @@
+//load data
+
+const loadData = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+
+    const data = await res.json();
+    console.log(data)
+}
+
+loadData()
