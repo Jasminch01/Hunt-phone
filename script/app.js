478857a18e18298428f86a34000f0a1abc2035a1
jasmin <jasminchakma895@gmail.com>
2023-08-28 18:17:13 +0600

    show details and dynamic modal open

--- script/app.js
+++ script/app.js
@@ -6,7 +6,7 @@ const showAllContainer = document.getElementById('show-all-container')
 const loading = document.getElementById('loading-container')
 
 //load data
-const loadData = async (searchText, isShowAll) => {
+const loadData = async (searchText = '13', isShowAll) => {
   const res = await fetch(
     `https://openapi.programming-hero.com/api/phones?search=${searchText}`
   );
@@ -44,7 +44,7 @@ const showData = (phones, isShowAll) => {
     </div> 
     <div class="flex justify-center items-center">
         <div>
-            <button
+            <button onclick = "handleShowDetails('${phones.slug}')"
             class="lg:p-3 p-3 md:p-3 bg-gradient-to-r from-[#A4BC46] to-[#85A019] text-white font-semibold border-0 rounded-xl"
           >
            Show Details
@@ -59,6 +59,40 @@ const showData = (phones, isShowAll) => {
   toggleLoading(false)
 };
 
+const handleShowDetails = async (id) => {
+  const res = await fetch (` https://openapi.programming-hero.com/api/phone/${id}`);
+
+  const data = await res.json();
+  console.log(data);
+
+  const phone = data.data;
+
+  //
+  showphonsDetails(phone)
+}
+
+const showphonsDetails = (phone) =>{
+  const showDetailsContainer = document.getElementById('show-details-container');
+  showDetailsContainer.innerHTML = `
+  <div class = 'flex justify-center'>
+  <img src="${phone.image}" alt="">
+  </div>
+  <p class = 'font-medium text-2xl mt-3'>${phone.name} </p>
+  <p class = ''>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+  <p> <span class = 'font-medium'>Storage :</span> ${phone?.mainFeatures?.storage}</p>
+  <p> <span class = 'font-medium'>Display : </span>${phone?.mainFeatures?.displaySize}</p>
+  <p> <span class = 'font-medium'>Chepset : </span> ${phone?.mainFeatures?.chipSet}</p>
+  <p> <span class = 'font-medium'>slug : </span> ${phone?.slug}</p>
+  <p> <span class = 'font-medium'>Relase Date : </span> ${phone?.releaseDate || 'not found' }</p>
+  <p> <span class = 'font-medium'>Brand: </span> ${phone?.brand}</p>
+  <p> <span class = 'font-medium'>Gps: </span> ${phone?.others?.GPS || 'not found'}</p>
+
+  `
+  console.log(phone)
+  //
+  show_details_modal.showModal();
+}
+
 const searchHandle = (isShowAll) => {
   const searchText = searchInput.value;
   if(searchText === ''){
@@ -68,6 +102,7 @@ const searchHandle = (isShowAll) => {
   }
     loadData(searchText, isShowAll);
 }
+loadData();
 
 const toggleLoading = (isLoading) =>{
   if (isLoading) {
